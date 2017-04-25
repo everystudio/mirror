3a03d4a6ff25fb68f2ad42a2bb74ea160ce5d4ed
watanabe.everystudio <watanabe.everystudio@gmail.com>
2017-04-25 14:53:38 +0900

    広告追加

---
+++ Assets/10script/AdsInterstential.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdsInterstential : MonoBehaviour {
+
+	[SerializeField]
+	string adUnitIdAndroid = "";
+	[SerializeField]
+	string adUnitIdIOS = "";
+
+	public enum SHOW_TYPE
+	{
+		COUNT	= 0,
+		RANDOM	
+	};
+	public SHOW_TYPE m_eShowType;
+
+	public int m_iShowIntervalCount;
+	public int m_iShowRequestCount;
+
+	[Range(0, 100)]
+	public int m_iShowRandomProb;
+
+	InterstitialAd interstitial;
+	private AdRequest request;
+
+	private bool m_bStandby;
+	// Use this for initialization
+	void Start () {
+		if (adUnitIdAndroid.Equals(""))
+		{
+			Debug.LogError("no set AdsInterstential.adUnitIdAndroid");
+		}
+		if (adUnitIdIOS.Equals(""))
+		{
+			Debug.LogError("no set AdsInterstential.adUnitIdIOS");
+		}
+
+		if (m_iShowIntervalCount < -1)
+		{
+			m_iShowIntervalCount = 0;
+		}
+		m_iShowRequestCount = 0;
+		Load();
+	}
+
+	private void Load()
+	{
+		if (m_bStandby)
+		{
+			Debug.LogError("Ready");
+			return;
+		}
+
+		string strUnitId = "";
+#if UNITY_ANDROID
+		strUnitId = adUnitIdAndroid;
+#elif UNITY_IOS
+		strUnitId = adUnitIdIOS;
+#endif
+		interstitial = new InterstitialAd(strUnitId);
+
+		request = new AdRequest.Builder()
+			.AddTestDevice("B58A62380C00BF9DC7BA75C756B5F550")
+			.AddTestDevice("30ec665ef7c68238905003e951174579")
+			.Build();
+
+		interstitial.LoadAd(request);
+		interstitial.OnAdLoaded += OnAdLoaded;
+		interstitial.OnAdClosed += OnAdClosed;
+
+	}
+
+	public bool Show()
+	{
+		if (m_eShowType == SHOW_TYPE.COUNT)
+		{
+			m_iShowRequestCount -= 1;
+			if (0 < m_iShowRequestCount)
+			{
+				return false;
+			}
+		}
+		else if(m_eShowType == SHOW_TYPE.RANDOM)
+		{
+			int iResult = UtilRand.GetRand(100);
+			if(m_iShowRandomProb < iResult)
+			{
+				return false;
+			}
+		}
+
+		if (interstitial.IsLoaded())
+		{
+			interstitial.Show();
+		}
+		else
+		{
+			Debug.LogError(" no loaded ");
+		}
+		return m_bStandby;
+	}
+
+	private void OnAdLoaded(object sender, System.EventArgs e)
+	{
+		m_bStandby = true;
+		/*
+		InterstitialAd inter = (InterstitialAd)sender;
+		inter.Show();
+		*/
+	}
+
+	// インタースティシャル広告を閉じた時に走る
+	void OnAdClosed(object sender, System.EventArgs e)
+	{
+		m_bStandby = false;
+		interstitial.Destroy();
+		interstitial = null;
+		Load();
+	}
+}
---
+++ Assets/10script/AdsManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using GoogleMobileAds.Api;
+
+public class AdsManager : Singleton<AdsManager>
+{
+	private AdsBanner m_adsBannerGameBottom;
+
+	void loadedScene(Scene scenename, LoadSceneMode SceneMode)
+	{
+		Debug.LogError("今のSceneの名前 = " + scenename.name);
+		//setup(scenename.name);
+	}
+
+	public void OnShowPage( string _strName)
+	{
+		Debug.Log(string.Format("pagename:{0}", _strName));
+		Show(_strName);
+	}
+	public override void Initialize()
+	{
+		m_adsBannerGameBottom = null;
+		//Debug.LogError(SceneManager.GetActiveScene().name);
+
+		Show(UIAssistant.main.GetCurrentPage());
+		UIAssistant.onShowPage += OnShowPage;
+		SceneManager.sceneLoaded += loadedScene;
+		Debug.LogError("AdsManager.Initialize");
+
+	}
+
+	public void OnSkitStarted()
+	{
+		ShowBanner(false);
+	}
+
+	public void OnSkitFinished()
+	{
+		ShowBanner(true);
+	}
+
+	private void cleanup()
+	{
+		if (m_adsBannerGameBottom != null)
+		{
+			Destroy(m_adsBannerGameBottom);
+			m_adsBannerGameBottom = null;
+		}
+	}
+
+	public void Show( string _strName )
+	{
+		if (m_adsBannerGameBottom == null)
+		{
+			m_adsBannerGameBottom = gameObject.AddComponent<AdsBanner>();
+		}
+		m_adsBannerGameBottom.Show();
+	}
+
+	private void setup(string _strSceneName)
+	{
+		cleanup();
+		switch (_strSceneName)
+		{
+			case "Command":
+			default:
+				if (m_adsBannerGameBottom == null)
+				{
+					m_adsBannerGameBottom = gameObject.AddComponent<AdsBanner>();
+				}
+				break;
+		}
+	}
+
+	public void ShowBanner(bool _bFlag)
+	{
+		if( m_adsBannerGameBottom == null)
+		{
+			return;
+		}
+		if (_bFlag)
+		{
+			m_adsBannerGameBottom.Show();
+		}
+		else
+		{
+			m_adsBannerGameBottom.Hide();
+		}
+	}
+}
+
